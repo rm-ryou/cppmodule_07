2528636920a6c3e718b962ec7dc2499de89961b1
rm-ryou <rmoriya@example.com>
2022-10-15 21:03:21 +0900

    add ex02

---
+++ ex02/srcs/Array.h
@@ -0,0 +1,49 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <cstdlib>
+#include <iostream>
+
+template<class T>
+class Array {
+private:
+	T*	array_;
+	int	size_;
+public:
+	Array(): size_(0) {
+		array_ = new T[this->size_];
+	};
+	Array(int size): size_(size) {
+		array_ = new T[this->size_];
+	};
+	Array(const Array& array) {
+		std::cout << "Copy constructor called." << std::endl;
+		*this = array;
+	};
+	Array& operator=(const Array& other) {
+		if (this != &other) {
+			this->size_ = other.size_;
+			this->array_ = new T[this->size_];//other.array_;
+			for (int i = 0; i < this->size_; i++)
+				this->array_[i] = other.array_[i];
+		}
+		return *this;
+	};
+	~Array() {
+		std::cout << "Destructor called." << std::endl;
+		delete[] this->array_;
+	};
+	T& operator[](const int i) {
+		if (i < 0 || i >= this->size_)
+			throw Array::ArrayException();
+		return (this->array_[i]);
+	};
+	class ArrayException: public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "invalid index.";
+			};
+	};
+};
+
+#endif /* ARRAY_H */
